81fab7954fb953bec3d8c00ffc55153f1f0587d2
EzeOl123 <ezeolivera002@gmail.com>
2025-09-06 11:40:25 +0200

    Intento 1

--- src/comps/Cards.jsx
+++ src/comps/Cards.jsx
@@ -4,17 +4,21 @@ import Card from "./Card";
 const Cards = ({ data, setCitystate }) => {
   return (
     <div className="flex flex-row gap-6 items-center justify-center font-poppins">
-      {data.map((x, index) => {
-        return (
-          <Card
-            key={index}
-            name={x?.location?.name ?? "AA"}
-            temp={x?.current?.temp_c ?? "AA"}
-            icon={x?.current?.condition?.icon}
-            setCitystate={setCitystate}
-          />
-        );
-      })}
+      {data ? (
+        data.map((x, index) => {
+          return (
+            <Card
+              key={index}
+              name={x?.location?.name ?? "AA"}
+              temp={x?.current?.temp_c ?? "AA"}
+              icon={x?.current?.condition?.icon}
+              setCitystate={setCitystate}
+            />
+          );
+        })
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
